3f73f7ae85acc8cfdf888f76ee2d6e9e9f34dec4
Mickaelly Rodrigues <mickaellyrodrigues@MacBook-Air-de-Mickaelly.local>
2024-12-17 22:11:52 -0300

    Initial project bones in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
