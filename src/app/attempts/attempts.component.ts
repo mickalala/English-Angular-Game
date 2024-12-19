a97782df0fd215fa53ea88cd1e2b3e384020de77
Mickaelly Rodrigues <mickaellyrodrigues@MacBook-Air-de-Mickaelly.local>
2024-12-18 21:30:48 -0300

    feat: progress

---
+++ src/app/attempts/attempts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attempts',
+  templateUrl: './attempts.component.html',
+  styleUrl: './attempts.component.css'
+})
+export class AttemptsComponent {
+
+}
