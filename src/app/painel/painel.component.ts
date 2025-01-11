223a28e0e736077ed821aa46d04ec74e26d2964a
Mickaelly Rodrigues <mickalarissa01@gmail.com>
2025-01-11 17:41:30 -0300

    exchanging rounds and comparing answers

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -8,8 +8,32 @@ import { PHRASES } from './phrases.mock';
   styleUrl: './painel.component.css'
 })
 export class PainelComponent {
+
   public phrases: Phrase[] = PHRASES
+  public instruction: string = 'Traduza essa frase:'
+  public answer: string | null = ''
+
+  public round: number = 0
+  public roundPhrase: Phrase = this.phrases[0]
+
   constructor() {
-    // console.log(this.phrases)
+    this.roundPhrase = this.phrases[this.round]
+  }
+
+  public updateAnswer(userAnswer: Event): void {
+    this.answer = (<HTMLInputElement>userAnswer.target).value
+    console.log(this.answer)
+  }
+
+  public compareAnswer(): void {
+    if (this.answer == this.roundPhrase.ptbrPhrase) {
+
+      alert('Resposta correta!')
+
+      this.round++
+      this.roundPhrase = this.phrases[this.round]
+    } else {
+      alert('Resposta incorreta!')
+    }
   }
 }
--- src/app/painel/phrases.mock.ts
+++ src/app/painel/phrases.mock.ts
@@ -1,8 +1,9 @@
 import { Phrase } from "../shared/phrase.model";
 
 export const PHRASES: Array<Phrase> = [
+    { engPhrase: "Hi", ptbrPhrase: "Oi" },
     { engPhrase: "I don't like you", ptbrPhrase: "Eu não gosto de você" },
-    { engPhrase: "I like popcorn", ptbrPhrase: "EU gosto de pipoca" },
+    { engPhrase: "I like popcorn", ptbrPhrase: "Eu gosto de pipoca" },
     { engPhrase: "Do you wanna meet?", ptbrPhrase: "você quer se encontrar?" },
     { engPhrase: "Spicy in the ass of the others is juicy", ptbrPhrase: "Pimenta no cu dos outros é refresco" },
     { engPhrase: "don't give stage for crazy people", ptbrPhrase: "Não dê palco pra maluco" },
